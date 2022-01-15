f3341718799a9f44cf3406fc411463fa38708ca7
Ricardo <ricardomg92@hotmail.com>
2022-01-15 15:55:42 +0100

    Completed the project

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,9 +1,19 @@
+require "pry-byebug"
+
 def stock_picker(arr)
+  #binding.pry
+  best_days = []
+  best_profit = 0
   arr.each_index do |buy_day|
     arr.each_index do |sell_day|
-      puts "#{buy_day} #{sell_day}"
+      if sell_day > buy_day and arr[sell_day] - arr[buy_day] > best_profit
+        best_profit = arr[sell_day] - arr[buy_day]
+        best_days[0] = buy_day
+        best_days[1] = sell_day
+      end
     end
   end
+  puts best_days
 end
 
 stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
